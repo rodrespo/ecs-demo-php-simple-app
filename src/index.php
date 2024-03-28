62b89873e099dd102de0b7dc33eb93f411994a34
rodrespo <rodesposito@gmail.com>
2024-03-28 15:33:47 +0000

    index.html novo

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <h1>DevOps Engineering on AWS</h1>
+                <h2>Demonstração Pipeline Containers</h2>
+                <p>Nossa aplicação esta sendo executada no Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
